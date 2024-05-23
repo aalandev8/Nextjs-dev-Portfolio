a004a5be3d65035213fe75f993789fb240d8e918
aalandev8 <aalan.dev8@gmail.com>
2024-05-23 20:47:21 -0300

    Fix: removed labels and added placeholders instead

--- app/_components/ContactMe.tsx
+++ app/_components/ContactMe.tsx
@@ -2,26 +2,21 @@
 
 export const ContactMe = () => {
   return (
-    <section className="py-12 bg-gray-100">
+    <section className="py-12 ">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
-        
         <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0">
-          <form className="bg-white p-6 rounded-lg shadow-lg">
+          <form className=" p-6 ">
             <div className="mb-4">
-              <label htmlFor="name" className="block text-[#404040] font-bold mb-2">Name</label>
-              <input type="text" id="name" name="name" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="text" id="name" name="name" placeholder="Your name" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <label htmlFor="email" className="block text-[#404040] font-bold mb-2">Email</label>
-              <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="email" placeholder="Email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <label htmlFor="page" className="block text-[#404040] font-bold mb-2">Page (if exist)</label>
-              <input type="text" id="page" name="page" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="text" placeholder="Your page (if exist)" id="page" name="page" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <label htmlFor="help" className="block text-[#404040] font-bold mb-2">How can I help?</label>
-              <textarea id="help" name="help" className="w-full px-3 py-2 border rounded-lg focus:outline-none" rows={4}></textarea>
+              <textarea id="help" name="help" placeholder="How can i Help?*" className="w-full px-3 py-2 border rounded-lg focus:outline-none" rows={4}></textarea>
             </div>
             <div>
               <button type="submit" className="w-full bg-black text-white py-2 rounded-lg">Submit</button>
@@ -30,7 +25,7 @@ export const ContactMe = () => {
         </div>
         
         <div className="w-full md:w-1/2 md:pl-8 text-center md:text-left">
-          <h2 className="text-4xl font-bold mb-4">Let's Talk Together</h2>
+          <h2 className="text-4xl font-bold mb-4">Let&#8217;s <span className="border-text">Talk </span>for <br></br> Something special</h2>
           <p className="text-lg text-[#404040] mb-4">
             I'm excited to collaborate on new projects and help bring your ideas to life. Whether you have a question or just want to say hi, feel free to reach out.
           </p>

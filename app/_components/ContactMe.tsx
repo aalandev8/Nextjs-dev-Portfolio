309ff7365650f59c239944c64c143699ba4fc7f7
aalandev8 <aalan.dev8@gmail.com>
2024-05-23 21:09:06 -0300

    Fix: Updated mail to action to get in contact to my mail, changed some details on contact button

--- app/_components/ContactMe.tsx
+++ app/_components/ContactMe.tsx
@@ -7,19 +7,19 @@ export const ContactMe = () => {
         <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0">
           <form className=" p-6 ">
             <div className="mb-4">
-              <input type="text" id="name" name="name" placeholder="Your name" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="text" id="name" name="name" placeholder="Your name" className="w-full px-3 py-2 border-2 border-black rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <input type="email" placeholder="Email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="email" placeholder="Email" id="email" name="email" className="w-full px-3 py-2 border-2 border-black rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <input type="text" placeholder="Your page (if exist)" id="page" name="page" className="w-full px-3 py-2 border rounded-lg focus:outline-none" />
+              <input type="text" placeholder="Your page (if exist)" id="page" name="page" className="w-full px-3 py-2 border-2 border-black rounded-lg focus:outline-none" />
             </div>
             <div className="mb-4">
-              <textarea id="help" name="help" placeholder="How can i Help?*" className="w-full px-3 py-2 border rounded-lg focus:outline-none" rows={4}></textarea>
+              <textarea id="help" name="help" placeholder="How can i Help?*" className="w-full px-3 py-2 border-2 border-black rounded-lg focus:outline-none" rows={4}></textarea>
             </div>
             <div>
-              <button type="submit" className="w-full bg-black text-white py-2 rounded-lg">Submit</button>
+              <button type="submit" className="px-8 z-30 py-4 bg-black rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-[#404040] after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-500 after:transition-all after:duration-500 transition-all duration-500 text-2xl">Submit</button>
             </div>
           </form>
         </div>
@@ -30,7 +30,7 @@ export const ContactMe = () => {
             I'm excited to collaborate on new projects and help bring your ideas to life. Whether you have a question or just want to say hi, feel free to reach out.
           </p>
           <p className="text-lg text-[#404040]">
-        <a href="mailto:your.email@example.com" className="text-black font-bold">aalan.dev8@gmail.com</a>
+        <a href="mailto:aalan.dev8@gmail.com" className="text-black font-bold underline">aalan.dev8@gmail.com</a>
           </p>
         </div>
         

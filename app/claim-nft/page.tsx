8edaa2df8cb3a78308a816cda8371aaf5a44db22
aalandev8 <aalan.dev8@gmail.com>
2024-08-27 18:11:02 -0300

    Fix: created claim page

---
+++ app/claim-nft/page.tsx
@@ -0,0 +1,7 @@
+export default function ClaimNFT() {
+  return (
+    <section className="w-full h-screen">
+        <h1 className="text-bold text-3xl text-black">WORKS!</h1>
+    </section>
+  );
+}

bc3d1b57dddf292db97c5f3d1b056f19ffc50b23
aalandev8 <aalan.dev8@gmail.com>
2024-08-27 18:40:35 -0300

    Fix: added margin top to all main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import {Hero, Skills, Experiencie, AboutMe, MyProjects, ContactMe} from '@/app/_
 
 export default function Home() {
   return (
-    <>
+    <section className='mt-10 md:mt-0'>
       <Hero />
       <Skills />
       <Experiencie />
       <AboutMe />
       <MyProjects />
       <ContactMe />
-    </>
+    </section>
   );
 }
